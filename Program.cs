9d70d66937a994f708aff2640abec93fec461370
layla-p <layla.porter@gmail.com>
2019-06-06 19:21:14 +0100

    demo of pulling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LearnDotnetConsole
 {
@@ -6,9 +7,62 @@ namespace LearnDotnetConsole
     {
         private static void Main()
         {
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("How do you like your coffee?");
+                        Console.WriteLine("What is your name?");
+
+            Console.WriteLine("What are your favourite foods");
+
+            var foodString = Console.ReadLine();
             
+            var list = GetFoodList(foodString); 
+
+            Console.WriteLine($"My favourite food is { PickAFood(list)}");
+
             Console.ReadKey();
+        }
+
+        private static int CheckAge(string ageString)
+        {
+            int age;
+
+            int.TryParse(ageString, out age);
+
+            if(age == 0)
+            {
+                Console.WriteLine("Please give me a number");
+                ageString = Console.ReadLine();
+                int.TryParse(ageString, out age);
+            }
+
+            return age;
+        }
+
+        private static List<string> GetFoodList(string foodString)
+        {
+            
+            var foodList = new List<string>();
+
+            var foodArray = foodString.Split(" ");
+
+            foreach(var food in foodArray)
+            {
+                foodList.Add(food);
+            }
+
+
+           return foodList; 
+        }
+
+        private static string PickAFood(List<string> list)
+        {
+            var count = list.Count;
+
+            var r = new Random();
+
+            var indexNumber = r.Next(count-1);
+
+            return list[indexNumber];
+
         }
     }
 }
